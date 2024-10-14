e24181e8ea8c14f946bf168e29c5ec148b1c5be7
Jacky Le <jacky.le408@gmail.com>
2024-10-14 15:25:57 -0400

    stack card component

--- src/lib/components/reusable-components/StackCard.svelte
+++ src/lib/components/reusable-components/StackCard.svelte
@@ -1,62 +1,72 @@
 <script>
-import '../../global-css/utils.css';
+  import "../../global-css/utils.css";
 
-export let imageSrc = '';
-export let title = '';
-export let useCase = '';
-export let description = '';
-title = title.toUpperCase();
-useCase = useCase.toUpperCase();
+  export let imageSrc = "";
+  export let title = "";
+  export let useCase = "";
+  export let description = "";
+  title = title.toUpperCase();
+  useCase = useCase.toUpperCase();
 </script>
 
-<figure class="card br p-xl bg-white-200 shadow-md">
-    <img src={imageSrc} alt={title} class="card__image" />
+<figure class="card br p-lg bg-white-200 border">
+  <img src={imageSrc} alt={title} class="card__image bg-white-100 br border" />
 
-    <figcaption class="card__caption">
-        <h4 class="card__title  ff-bold fw-bold fsz-lg text-black-800 lh-lg">{title}</h4>
-        <h5 class="caption__useCase  ff-bold fw-bold fsz-3xs  text-800 lh-3xs ">({useCase})</h5>
-        <p class="card__description ff-bold fw-medium fsz-xs text-600 lh-md">{description}</p>
-    </figcaption>
+  <figcaption class="card__content p-base bg-white-100 br border">
+    <h4 class="card__title ff-bold fw-bold fsz-md text-800 lh-md">{title}</h4>
+    <h5 class="card__useCase ff-bold fw-bold fsz-xs text-800 lh-xs">
+      {useCase}
+    </h5>
+    <p class="card__description ff-regular fw-medium fsz-xs text-gray lh-2xl">
+      {description}
+    </p>
+  </figcaption>
 </figure>
 
 <style>
-*,
-*::before,
-*::after {
+  *,
+  *::before,
+  *::after {
     box-sizing: border-box;
+  }
 
-}
-
-.card {
+  .card {
     display: flex;
-    flex-direction: column;
     border: 0.625rem solid var(--color-text-400);
     justify-content: center;
     width: 100%;
     height: 100%;
     overflow: hidden;
-    max-width: 28.125rem;
-
+    max-width: 52.875rem;
     gap: var(--fsz-md);
 
-    & .card__counter {
-
-        letter-spacing: 0.15rem;
-        line-height: 1;
-        margin-bottom: -0.125rem;
-
-    }
-
     & .card__title {
-
-        display: flex;
-        align-content: center;
-        align-items: center;
-        width: 100%;
-        gap: var(--spacing-sm);
-        letter-spacing: -0.0125em;
-        text-transform: capitalize;
+      display: flex;
+      align-content: center;
+      align-items: center;
+      width: 100%;
+      gap: var(--spacing-sm);
+      letter-spacing: -0.0125em;
+      text-transform: capitalize;
     }
 
-}
+    & .card__content {
+      display: flex;
+      flex-direction: column;
+      gap: 6px;
+    }
+
+    & .card__image {
+      align-self: start;
+      padding: 0.875rem;
+    }
+  }
+
+  .border {
+    border: 2px solid #00000008;
+  }
+
+  .text-gray {
+    color: #1d1d1fb3;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,23 @@
 <script>
-  import ServicesSection from '../lib/components/section-components/ServicesSection.svelte';
-  import ProjectsSection from '../lib/components/section-components/ProjectsSection.svelte';
-  import HeroSection from '../lib/components/section-components/HeroSection.svelte';
-  import ImageCard from '../lib/components/reusable-components/ImageCard.svelte';
+  import ServicesSection from "../lib/components/section-components/ServicesSection.svelte";
+  import ProjectsSection from "../lib/components/section-components/ProjectsSection.svelte";
+  import HeroSection from "../lib/components/section-components/HeroSection.svelte";
+  import ImageCard from "../lib/components/reusable-components/ImageCard.svelte";
+  import StackSection from "../lib/components/section-components/StackSection.svelte";
 </script>
 
 <svelte:head>
   <title>My Modern Site</title>
-  <meta name="description" content="Refresh your web presence with our expert web development services tailored for B2B enterprises. Our agency specializes in creating modern, scalable, and innovative digital solutions to drive your business forward. Partner with us for expert web design, development, and digital marketing strategies that deliver measurable results." />
-
+  <meta
+    name="description"
+    content="Refresh your web presence with our expert web development services tailored for B2B enterprises. Our agency specializes in creating modern, scalable, and innovative digital solutions to drive your business forward. Partner with us for expert web design, development, and digital marketing strategies that deliver measurable results."
+  />
 </svelte:head>
 <!-- <HeroSection /> -->
 <!-- <ProjectsSection /> -->
 <!-- <ImageCard /> -->
-<ServicesSection />
+<!-- <ServicesSection /> -->
+<StackSection />
 
 <style>
-  
 </style>
