c1a0281a1a47ac32cdb0abcf6a33ceb75df2974d
Jacky Le <jacky.le408@gmail.com>
2024-10-15 13:08:29 -0400

    stack done

--- src/lib/components/reusable-components/StackCard.svelte
+++ src/lib/components/reusable-components/StackCard.svelte
@@ -9,19 +9,27 @@
   useCase = useCase.toUpperCase();
 </script>
 
-<figure class="card br p-lg bg-white-200 border">
-  <img src={imageSrc} alt={title} class="card__image bg-white-100 br border" />
+<div class="card__wrapper">
+  <figure class="card br p-base bg-white-200 border">
+    <div class="card__image-wrapper bg-white-100 br border">
+      <img
+        src={imageSrc}
+      alt={title}
+        class="card__image"
+      />
+    </div>
 
-  <figcaption class="card__content p-base bg-white-100 br border">
-    <h4 class="card__title ff-bold fw-bold fsz-md text-800 lh-md">{title}</h4>
-    <h5 class="card__useCase ff-bold fw-bold fsz-xs text-800 lh-xs">
-      {useCase}
-    </h5>
-    <p class="card__description ff-regular fw-medium fsz-xs text-gray lh-2xl">
-      {description}
-    </p>
-  </figcaption>
-</figure>
+    <figcaption class="card__content p-base bg-white-100 br border">
+      <h4 class="card__title ff-bold fw-bold fsz-md text-800 lh-md">{title}</h4>
+      <h5 class="card__useCase ff-bold fw-bold fsz-xs text-800 lh-xs">
+        {useCase}
+      </h5>
+      <p class="card__description ff-regular fw-medium fsz-xs text-gray lh-2xl">
+        {description}
+      </p>
+    </figcaption>
+  </figure>
+</div>
 
 <style>
   *,
@@ -30,14 +38,16 @@
     box-sizing: border-box;
   }
 
+  .card__wrapper {
+    container-type: inline-size;
+  }
+
   .card {
     display: flex;
     border: 0.625rem solid var(--color-text-400);
-    justify-content: center;
-    width: 100%;
-    height: 100%;
+    justify-content: between;
+    width: fit-content;
     overflow: hidden;
-    max-width: 52.875rem;
     gap: var(--fsz-md);
 
     & .card__title {
@@ -56,8 +66,9 @@
       gap: 6px;
     }
 
-    & .card__image {
+    & .card__image-wrapper {
       align-self: start;
+      flex-shrink: 0;
       padding: 0.875rem;
     }
   }
@@ -68,5 +79,19 @@
 
   .text-gray {
     color: #1d1d1fb3;
+  }
+
+  @container (max-width: 475px) {
+    .card {
+      flex-direction: column;
+    }
+
+    .card__image-wrapper {
+      width: 100%;
+    }
+
+    .card__image {
+      margin-inline: auto;
+    }
   }
 </style>
