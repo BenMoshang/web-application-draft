fec451dca64329ef6290af4544f788c8f3e29990
Ben Moshang <Benmoshang@gmail.com>
2024-10-16 09:34:12 -0400

    d

--- src/lib/components/section-components/HeroSection.svelte
+++ src/lib/components/section-components/HeroSection.svelte
@@ -1,13 +1,13 @@
 <script>
-
+  let title = "Modernized <br /> Web Presence";
+  title = title.toUpperCase();
 </script>
 
 <section class="hero-section">
+  <h1 class="global__title--xl2">{@html title}</h1>
 
-
-
-
+  <button class="global__button--solid">Get Started</button>
 </section>
+
 <style>
-    
 </style>
