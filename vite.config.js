6fc884e6b32334c67e70294fff76a8f9237541a8
Ben Moshang <benmoshang@gmail.com>
2024-11-07 12:16:59 -0500

    you are now able to host to your phone and ip but if you want local host just comment out host and port

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			// Restrict file access outside the project root for better security
-			allow: ['..'],
-		},
-	},
-	build: {
-		// To improve performance by reducing the size of the built files
-		minify: 'esbuild',
-		target: 'esnext',
-	},
-	optimizeDeps: {
-		// Optimize SvelteKit and Vite dependencies for better build times
-		exclude: ['svelte'],
-	},
+  plugins: [sveltekit()],
+  server: {
+    fs: {
+      // Restrict file access outside the project root for better security
+      allow: [".."],
+    },
+    host: "10.0.0.85",
+    port: 5173,
+  },
+  build: {
+    // To improve performance by reducing the size of the built files
+    minify: "esbuild",
+    target: "esnext",
+  },
+  optimizeDeps: {
+    // Optimize SvelteKit and Vite dependencies for better build times
+    exclude: ["svelte"],
+  },
 });
