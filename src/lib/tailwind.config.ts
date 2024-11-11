f46bfba442fae3258b9f6dfab498fbcfd5938ace
Ben Moshang <benmoshang@gmail.com>
2024-11-11 15:42:01 -0500

    c

---
+++ src/lib/tailwind.config.ts
@@ -0,0 +1,24 @@
+import svgToDataUri from "mini-svg-data-uri";
+import type { Config } from "tailwindcss";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
+const config: Config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [addVariablesForColors],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
