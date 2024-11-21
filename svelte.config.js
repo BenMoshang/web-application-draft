5616ef25810189acb00ae832746dd2981d274cda
Ben Moshang <benmoshang@gmail.com>
2024-11-21 11:35:03 -0500

    fixed pre process

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
 import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    paths: {
-      assets: "/static",
-      base: "",
-    },
   },
-  preprocess: vitePreprocess(),
+  preprocess: sveltePreprocess({
+    scss: {
+      includePaths: ["src"],
+    },
+  }),
 };
 
 export default config;
